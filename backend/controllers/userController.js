ef02f18aa2eb1c44b7734b4b0e633fe0f873c1bb
Ambre-guia <ambreguia77@gmail.com>
2024-08-30 11:43:31 +0200

    rename update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,8 @@
 const User = require('../models/User');
 
 exports.getUser = async (req, res) => {
-  console.log('là');
   try {
     const user = await User.findById(req.params.userId);
-    //const user = await User.findOne({ email });
-    console.log(user);
     res.json({ user });
   } catch (err) {
     res.status(400).json({ error: err.message });
